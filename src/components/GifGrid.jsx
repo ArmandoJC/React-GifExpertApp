edc31c0c26dacafae049f5aac6e5a6000611275e
Slater <armando.jaimes@pucp.edu.pe>
2023-01-29 13:21:36 -0500

    Demostración de producción rápido

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,12 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  
-  const [counter, setCounter] = useState(10);
 
   useEffect( () => {
     getGifs(category);
 
   }, [] );
-
 
   return (
     <>
