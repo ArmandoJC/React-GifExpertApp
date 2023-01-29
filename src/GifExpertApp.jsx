66f4d6c12f5f16d348a6b152ff6a8c2e610aa05c
Slater <armando.jaimes@pucp.edu.pe>
2023-01-28 20:37:31 -0500

    Agregar una nueva categoría

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,19 +1,36 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon ball"]);
+
+  const onAddCategory = (event) => {
+    //Valorant
+    console.log("Entré a la función");
+    const newCategory = "Valorant";
+
+    setCategories([newCategory, ...categories]);
+    // setCategories((categories) => {
+    //   return [...categories, newCategory];
+    // });
+  };
+
   return (
     <>
-    {/* Título */}
+      {/* Título */}
       <h1>GifExpertApp</h1>
-    {/* Input */}
+      {/* Input */}
 
-
-    {/* Listado de Gif */}
+      {/* Listado de Gif */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          // return <li>Hola</li>
+          return <li key={category}>{category}</li>;
+        })}
+        {/* <li>ABC</li> */}
+      </ol>
 
       {/* Gif Item */}
-
     </>
-  )
-}
-
-
-
+  );
+};
